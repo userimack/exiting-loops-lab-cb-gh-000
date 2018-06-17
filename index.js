d102bb9c5545203619ba23ac9d3520c099c2b158
Mahendra Yadav <mahendra.k12@gmail.com>
2018-06-17 13:04:37 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,10 @@
+function breakOut(array, changeValue, stopValue) {
+  array.forEach((element, index, array) => {
+    if (element === stopValue){
+      break;
+    }
+    else{
+      array[index] = changeValue;
+    }
+  })
+}
